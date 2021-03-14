c084b50dc8a4fd4a3fe92e90a31492b0794f84cd
LucaDiBona <76700950+LucaDiBona@users.noreply.github.com>
2021-03-14 23:50:12 +0000

    rename

---
+++ latexFile.py
@@ -0,0 +1,4 @@
+class latexFile():
+
+    def __init__(self, fileName: str) -> None:
+        self.fileName=fileName
--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from latexFile import latexFile
 running=True
 while running:
     command=input("> ") #TODO Replace with reasonable TUI (curses maybe)
