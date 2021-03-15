216c6ed8592dfbb65843579a070994916ab64b3c
LucaDiBona <76700950+LucaDiBona@users.noreply.github.com>
2021-03-15 00:19:18 +0000

    test.

--- latexFile.py
+++ latexFile.py
@@ -5,5 +5,5 @@ class latexFile():
         self.__f=open(fileName, "a+")
         self.__f.seek(0)
         self.__fContentsI=self.__f.read()
-        print(self.__fContentsI)
-        self.fContents=self.__fContentsI
+
+        sel
