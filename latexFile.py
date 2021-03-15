3f7bbe55677759e77c8bc019a42ebb579191ae76
LucaDiBona <76700950+LucaDiBona@users.noreply.github.com>
2021-03-15 00:12:14 +0000

    began init

--- latexFile.py
+++ latexFile.py
@@ -2,3 +2,8 @@ class latexFile():
 
     def __init__(self, fileName: str) -> None:
         self.fileName=fileName
+        self.__f=open(fileName, "a+")
+        self.__f.seek(0)
+        self.__fContentsI=self.__f.read()
+        print(self.__fContentsI)
+        self.fContents=self.__fContentsI
--- main.py
+++ main.py
@@ -2,3 +2,7 @@ from latexFile import latexFile
 running=True
 while running:
     command=input("> ") #TODO Replace with reasonable TUI (curses maybe)
+    x=latexFile("test.txt")
+
+    if command=="kill":
+        running = False
